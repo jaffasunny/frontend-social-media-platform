b032312a56ab8acada3cc1bc77704eaa2f07bded
jaffasunny <jaffer.sunny125@gmail.com>
2024-04-01 11:46:25 +0400

    testing

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,20 +7,17 @@ importScripts(
 	"https://www.gstatic.com/firebasejs/10.1.0/firebase-messaging-compat.js"
 );
 
-importScripts("environment.js");
-
-// const firebaseConfig = {
-// 	apiKey: NEXT_PUBLIC_apiKey,
-// 	authDomain: NEXT_PUBLIC_authDomain,
-// 	projectId: NEXT_PUBLIC_projectId,
-// 	storageBucket: NEXT_PUBLIC_storageBucket,
-// 	messagingSenderId: NEXT_PUBLIC_messagingSenderId,
-// 	appId: NEXT_PUBLIC_appId,
-// 	measurementId: NEXT_PUBLIC_measurementId,
-// };
-// console.log({ firebaseConfig });
+const firebaseConfig = {
+	apiKey: "AIzaSyCDg_XFrsAwiSfrgZg5iHPW_zusovRS-XM",
+	authDomain: "social-media-notificatio-4bb57.firebaseapp.com",
+	projectId: "social-media-notificatio-4bb57",
+	storageBucket: "social-media-notificatio-4bb57.appspot.com",
+	messagingSenderId: "783709613560",
+	appId: "1:783709613560:web:8ba41b3a0f9fafd48b7a0d",
+	measurementId: "G-KGK9VZJFM2",
+};
 // eslint-disable-next-line no-undef
-firebase.initializeApp(environment.firebase);
+firebase.initializeApp(firebaseConfig);
 // eslint-disable-next-line no-undef
 const messaging = firebase.messaging();
 
