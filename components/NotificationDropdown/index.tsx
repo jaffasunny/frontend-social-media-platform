7b821246c4f49a197890c84664d1b9ccae33714c
jaffasunny <jaffer.sunny125@gmail.com>
2024-04-01 10:36:10 +0400

    project completed

--- components/NotificationDropdown/index.tsx
+++ components/NotificationDropdown/index.tsx
@@ -16,29 +16,21 @@ const NotificationDropdown = ({ notifications }: Props) => {
 
 	const [isOpen, setIsOpen] = useState(false);
 
-	useEffect(() => {
-		if (isOpen) {
-			let timeout = setTimeout(() => {
-				console.log("clearing notifications");
-				if (notifications?.data?.length) viewNotifications();
-				setIsOpen(!isOpen);
-			}, 10000);
-			return () => clearTimeout(timeout);
-		}
-	}, [isOpen]);
-
-	console.log(
-		"Number(notifications?.data?.length)",
-		Number(notifications?.data?.length)
-	);
+	// useEffect(() => {
+	// 	if (isOpen) {
+	// 		let timeout = setTimeout(() => {
+	// 			if (notifications && notifications.data[0]?.notifications?.length)
+	// 				viewNotifications();
+	// 			setIsOpen(!isOpen);
+	// 			console.log("clearing notifications", isOpen);
+	// 		}, 10000);
+	// 		return () => clearTimeout(timeout);
+	// 	}
+	// }, [isOpen]);
 
 	return (
 		<div className='hs-dropdown relative inline-flex'>
 			<button
-				onClick={() => {
-					console.log("button clicked", isOpen);
-					setIsOpen(!isOpen);
-				}}
 				id='hs-dropdown-default'
 				type='button'
 				className='hs-dropdown-toggle inline-flex items-center gap-x-2 text-sm font-medium rounded-lg bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600'>
@@ -50,20 +42,32 @@ const NotificationDropdown = ({ notifications }: Props) => {
 				) : null}
 			</button>
 
-			{Number(notifications?.data?.length) > 0 && (
-				<div
-					className='hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-2 mt-2 dark:bg-gray-800 dark:border dark:border-gray-700 dark:divide-gray-700 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full'
-					aria-labelledby='hs-dropdown-default'>
-					{notifications?.data[0]?.notifications?.map((notification, index) => (
-						<text
-							key={index}
-							className='flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:focus:bg-gray-700'
-							href='#'>
-							{notification.body}
-						</text>
-					))}
-				</div>
-			)}
+			<div
+				className='hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-2 mt-2 dark:bg-gray-800 dark:border dark:border-gray-700 dark:divide-gray-700 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full'
+				aria-labelledby='hs-dropdown-default'>
+				{notifications && notifications?.data[0]?.notifications?.length ? (
+					notifications?.data[0]?.notifications?.map((notification, index) => (
+						<div key={index}>
+							<div className='flex justify-end'>
+								<button
+									className='hover:bg-red-200 p-1 rounded-lg'
+									onClick={async () => {
+										await viewNotifications();
+									}}>
+									seen
+								</button>
+							</div>
+							<p className='flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:focus:bg-gray-700'>
+								{notification.body}
+							</p>
+						</div>
+					))
+				) : (
+					<p className='flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:focus:bg-gray-700'>
+						No notifications
+					</p>
+				)}
+			</div>
 		</div>
 	);
 };
